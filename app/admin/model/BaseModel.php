fc65217e0ebcfa40e0c63338cfdb8ce4a5eca1a5
elfesyaesen <web.elfesyaesen@gmail.com>
2024-12-18 21:01:09 +0300

    model update

--- app/admin/model/BaseModel.php
+++ app/admin/model/BaseModel.php
@@ -1,47 +1,16 @@
 <?php
+
 namespace Admin\Model;
 
-use PDO;
-use PDOException;
+use System\Engine\Model;
 
-class Model
+class BaseModel extends Model
 {
-    protected $pdo = null;
-    
-    public function __construct() {
-        try {
-            $this->pdo = new PDO('sqlsrv:server='. \DB_HOST .';database='. \DB_NAME, \DB_USER, \DB_PASS, \DB_OPTIONS);
-        } catch (PDOException $e) {
-            error_log('Veritabanı hatası: ' . $e->getMessage() . PHP_EOL, 3,\APP_ROOT . '/error_log.php');
-            die($e->getMessage());
-        }
-    }
-
-    public function createTable($tableName, $columns)
+    public function users(): array | false
     {
-        $sql = "
-        DECLARE @result VARCHAR(100);
-        IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '".$tableName."')
-        BEGIN
-            SET @result = 'TABLO VAR.';
-        END
-        ELSE
-        BEGIN
-            CREATE TABLE $tableName (";
-        $sql .= implode(', ', array_map(function($column) {
-            return $column['name'] . ' ' . $column['type'];
-        }, $columns));
-        $sql .= ");
-            SET @result = 'TABLO EKLENDI.';
-        END;
-        SELECT @result AS Statement;";
+        $statement = $this->pdo->query('SELECT * FROM users');
+        $response = $statement->fetchAll(\PDO::FETCH_ASSOC);
 
-        try {
-            $stmt = $this->pdo->query($sql);
-            $result = $stmt->fetch(PDO::FETCH_ASSOC);
-            return $result;
-        } catch (PDOException $e) {
-            echo 'Table Creation Error: ' . $e->getMessage();
-        }
+        return $response ?: [];
     }
 }
--- app/catalog/model/BaseModel.php
+++ app/catalog/model/BaseModel.php
@@ -1,35 +1,16 @@
 <?php
 
-namespace Catalog\Model;
+namespace App\Catalog\Model;
 
 use System\Engine\Model;
 
 class BaseModel extends Model
 {
-
-    public function event(): array|false
+    public function users(): array | false
     {
-        $sql = "SELECT * FROM trEvent ORDER BY id";
-        $statement = $this->pdo->query($sql);
+        $statement = $this->pdo->query('SELECT * FROM users');
         $response = $statement->fetchAll(\PDO::FETCH_ASSOC);
-        return $response ?: false;
-    }
 
-    public function createEvent(array $args = []): int|false
-    {
-        $sql = "INSERT INTO V1_URETIM.dbo.trEvent (title,start,className,description) VALUES (:title,:start,:className,:description)";
-        $statement = $this->pdo->prepare($sql);
-        $statement->execute($args);
-        $response = $this->pdo->lastInsertId();
-        return $response ?: false;
-    }
-
-    public function editEvent(array $args = []): int|false
-    {
-        $sql = "UPDATE V1_URETIM.dbo.trEvent SET title=:title,start=:start,className=:className,description=:description WHERE id=:id";
-        $statement = $this->pdo->prepare($sql);
-        $statement->execute($args);
-        $response = $statement->rowCount();
-        return $response ?: false;
+        return $response ?: [];
     }
 }
